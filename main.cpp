f75c9a708bdeb3f0cf0df33438a3151d1f492d49
Dmitriy Fishman <fishman.code@gmail.com>
2015-11-19 09:53:54 -0500

    Added 'Append image' implementation

--- main.cpp
+++ main.cpp
@@ -5,6 +5,28 @@ using namespace cv;
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    Mat src, dst;
+    char src_filename[] = "../images/src.png",
+         dst_filename[] = "../images/dst.png";
+
+    src = imread(src_filename, CV_LOAD_IMAGE_COLOR);
+    dst = imread(dst_filename, CV_LOAD_IMAGE_COLOR);
+
+    if (!src.data) {
+        printf("Could not open the image file '%s'", src_filename);
+        return -1;
+    }
+
+    if (!dst.data) {
+        printf("Could not open the image file '%s'", dst_filename);
+        return -1;
+    }
+
+    Mat tmp(cvSize(src.cols + dst.cols, src.rows), CV_8UC3, Scalar(255, 255, 255));
+    src.copyTo(tmp(Rect(0, 0, src.cols, tmp.rows)));
+    dst.copyTo(tmp(Rect(src.cols, 0, dst.cols, dst.rows)));
+
+    imwrite("../images/src.png", tmp);
+
     return 0;
 }
